cca66ac58af4af994b4da7251ba829dc42fe2b1a
Edwin Hu <eddyhu@gmail.com>
2025-06-19 12:14:35 -0400

    update nvim and wezterm configs

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,7 +9,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte
 config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.integrated_title_button_style = "MacOsNative"
 config.integrated_title_button_color = "Auto"
 config.enable_scroll_bar = true
@@ -19,6 +19,25 @@ config.leader = { key = "s", mods = "CTRL", timeout_milliseconds = 1000 } -- Set
 
 local act = wezterm.action
 config.keys = {
+	-- Backscroll Copy Mode
+	{
+		key = "[",
+		mods = "LEADER",
+		action = wezterm.action.ActivateCopyMode,
+	},
+	-- Rename Tab
+	{
+		key = ",",
+		mods = "LEADER",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
 	-- Pane Selection
 	{
 		mods = "LEADER",
@@ -35,63 +54,18 @@ config.keys = {
 	{ mods = "LEADER", key = "j", action = act.ActivatePaneDirection("Down") },
 	{ mods = "LEADER", key = "k", action = act.ActivatePaneDirection("Up") },
 	{ mods = "LEADER", key = "l", action = act.ActivatePaneDirection("Right") },
-	-- Resurrect
+}
+
+-- SSH Domains
+config.ssh_domains = {
 	{
-		mods = "LEADER",
-		key = "w",
-		action = wezterm.action_callback(function(win, pane)
-			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
-		end),
-	},
-	{ mods = "LEADER", key = "W", action = resurrect.window_state.save_window_action() },
-	{ mods = "LEADER", key = "T", action = resurrect.tab_state.save_tab_action() },
-	{
-		mods = "LEADER",
-		key = "s",
-		action = wezterm.action_callback(function(win, pane)
-			resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
-			resurrect.window_state.save_window_action()
-		end),
-	},
-	{
-		mods = "LEADER",
-		key = "r",
-		action = wezterm.action_callback(function(win, pane)
-			resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id, label)
-				local type = string.match(id, "^([^/]+)") -- match before '/'
-				id = string.match(id, "([^/]+)$") -- match after '/'
-				id = string.match(id, "(.+)%..+$") -- remove file extention
-				local opts = {
-					relative = true,
-					restore_text = true,
-					on_pane_restore = resurrect.tab_state.default_on_pane_restore,
-				}
-				if type == "workspace" then
-					local state = resurrect.state_manager.load_state(id, "workspace")
-					resurrect.workspace_state.restore_workspace(state, opts)
-				elseif type == "window" then
-					local state = resurrect.state_manager.load_state(id, "window")
-					resurrect.window_state.restore_window(pane:window(), state, opts)
-				elseif type == "tab" then
-					local state = resurrect.state_manager.load_state(id, "tab")
-					resurrect.tab_state.restore_tab(pane:tab(), state, opts)
-				end
-			end)
-		end),
-	},
-	{
-		mods = "LEADER",
-		key = "d",
-		action = wezterm.action_callback(function(win, pane)
-			resurrect.fuzzy_loader.fuzzy_load(win, pane, function(id)
-				resurrect.state_manager.delete_state(id)
-			end, {
-				title = "Delete State",
-				description = "Select State to Delete and press Enter = accept, Esc = cancel, / = filter",
-				fuzzy_description = "Search State to Delete: ",
-				is_fuzzy = true,
-			})
-		end),
+		name = "wrds",
+		remote_address = "wrds-cloud-sshkey.wharton.upenn.edu",
+		username = "eddyhu",
+		remote_wezterm_path = "~/.local/bin/wezterm",
+		ssh_option = {
+			identityfile = "~/.ssh/wrds_rsa",
+		},
 	},
 }
 
